ccc5dd91d47d0579615c62bfe03cbecd94faf0c3
haciyev13 <ehedhaciyev001@gmail.com>
2023-05-15 11:19:36 +0400

    WebSite Add

---
+++ 24.Web Site/main.js
@@ -0,0 +1,18 @@
+// function toggleMenu() {
+//   var navbarMenu = document.querySelector(".navbar-menu");
+//   var overlay = document.getElementById("overlay");
+
+//   navbarMenu.classList.toggle("active");
+//   overlay.classList.toggle("active");
+// }
+
+// function toggleOverlay() {
+//   var overlay = document.getElementById("overlay");
+//   overlay.classList.toggle("active");
+// }
+
+// var menuIcon = document.getElementById("menu-icon");
+// menuIcon.addEventListener("click", function () {
+//   toggleMenu();
+//   toggleOverlay();
+// });
